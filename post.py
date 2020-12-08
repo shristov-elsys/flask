547d4f99766b411aa757580c7dbbd485ef0888ec
Stoyan Hristov <shristov@elsys-bg.org>
2020-12-08 23:33:32 +0200

    Add comments 	- edit post 	- delete post

--- post.py
+++ post.py
@@ -1,4 +1,5 @@
 from database import DB
+from comment import Comment
 
 
 class Post:
@@ -27,3 +28,16 @@ class Post:
             values = (self.name, self.author, self.content)
             row = db.execute('INSERT INTO posts (name, author, content) VALUES (?, ?, ?)', values)
             return self
+
+    def save(self):
+        with DB() as db:
+            values = (self.name, self.author, self.content, self.post_id)
+            db.execute('UPDATE posts SET name = ?, author = ?, content = ? WHERE id = ?', values)
+            return self
+
+    def delete(self):
+        with DB() as db:
+            db.execute('DELETE FROM posts WHERE id = ?', (self.post_id,))
+
+    def comments(self):
+        return Comment.find_by_post(self)
