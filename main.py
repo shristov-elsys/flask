25111745695b5153263770284552d17c3954c4db
Stoyan Hristov <shristov@elsys-bg.org>
2020-12-08 23:18:56 +0200

    Use real life DB 	- change id to post_id

--- main.py
+++ main.py
@@ -4,27 +4,43 @@ from post import Post
 
 app = Flask(__name__)
 
-db = {1: Post(1, 'Post 1', 'Gosho', 'Content 1')}
-
 @app.route('/')
 def hello_world():
-    return 'Hello, World!!!'
+    return redirect(url_for('list_posts'))
+
 
 @app.route('/posts')
 def list_posts():
-    return render_template('posts.html', posts=db.values())
+    return render_template('posts.html', posts=Post.all())
 
-@app.route('/posts/<int:id>')
-def show_post(id):
-    post = db[id]
+
+@app.route('/posts/<int:post_id>')
+def show_post(post_id):
+    post = Post.find(post_id)
+
     return render_template('post.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit')
+def edit_post(post_id):
+    post = Post.find(post_id)
+
+    return render_template('post.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/delete', methods=['POST'])
+def delete_post(post_id):
+    post = Post.find(post_id)
+    post.delete()
+
+    return redirect(url_for('list_posts'))
+
 
 @app.route('/posts/new', methods=['GET', 'POST'])
 def new_post():
     if request.method == 'GET':
         return render_template('new_post.html')
     elif request.method == 'POST':
-        next_id = max(db.keys()) + 1
-        post = Post(next_id, request.form['name'], request.form['author'], request.form['content'])
-        db[next_id] = post
+        values = (None, request.form['name'], request.form['author'], request.form['content'])
+        Post(*values).create()
         return redirect(url_for('list_posts'))
